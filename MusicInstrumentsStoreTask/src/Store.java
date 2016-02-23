e2137db0b897a1fa95d4e5f8b8d593936cbbbac4
coasterka <tsvetanovah@gmail.com>
2016-02-23 19:43:49 +0200

    Removed unnecessary code

--- MusicInstrumentsStoreTask/src/Store.java
+++ MusicInstrumentsStoreTask/src/Store.java
@@ -55,21 +55,6 @@ public class Store implements IStore {
 					}
 				}
 			}
-
-			// for (TreeSet<MusicInstrument> instrumentsList :
-			// this.productsCatalog.values()) {
-			// for (MusicInstrument musicInstrument : instrumentsList) {
-			// if (musicInstrument.getQuantity() == 0) {
-			// try {
-			// musicInstrument.setQuantity(NUMBER_OF_DELIVERED_INSTRUMENTS);
-			// System.err.println("Delivered " + NUMBER_OF_DELIVERED_INSTRUMENTS
-			// + " of " + musicInstrument.getName());
-			// } catch (MusicStoreException e) {
-			// e.printStackTrace();
-			// }
-			// }
-			// }
-			// }
 		}
 	}
 
@@ -349,7 +334,6 @@ public class Store implements IStore {
 				musicInstrument.setQuantity((int) (Math.random() * MAX_STORE_PRODUCT_QUANTITY));
 			}
 		}
-
 	}
 
 	public double getMoney() {
@@ -362,5 +346,4 @@ public class Store implements IStore {
 		}
 		this.supplier = supplier;
 	}
-
 }
